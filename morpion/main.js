ee78b63e50262a8dd13f15dba5b5511eef7eccf2
Thomas Salmon <th_s@hotmail.fr>
2013-01-25 18:59:38 +0100

    .

--- morpion/main.js
+++ morpion/main.js
@@ -1,9 +1,18 @@
 var tableau= [];
 var i;
 
+function CoupPC()
+{
+    
+}
+
 function CoupJoueur(x)
 {
-    x.innerHTML="X";
+    if(x.innerHTML == "")	
+    {
+	x.innerHTML="X";
+	CoupPC();
+    }
 }
 
 for(i = 1; i < 10; i++)
@@ -11,3 +20,10 @@ for(i = 1; i < 10; i++)
     tableau[i] = document.getElementById("case"+i);
     tableau[i].addEventListener("click",CoupJoueur.bind(this, tableau[i]),false);
 } 
+
+alert("syntaxe correcte");
+/*
+1 2 3
+4 5 6 
+7 8 9
+*/
