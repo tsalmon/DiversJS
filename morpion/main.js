b3b54746b07f7499723d30d2002fad7d7513c08f
Thomas Salmon <th_s@hotmail.fr>
2013-01-25 23:43:13 +0100

    [morpion] probleme de syntaxe

--- morpion/main.js
+++ morpion/main.js
@@ -50,19 +50,31 @@ function CoupJoueur(x,y)
     }
 }
 
-document.getElementById("jeu").innerHTML = "<div><a id=\"choix1\" href=\"\">Jouer contre l'ordi</a><a id=\"choix2\" href=\"\">2 Joueurs</a></div>";
 
-choix1.addEventListener("click",,false);
-choix2.addEventListener("click",function(){document.getElementById("jeu").innerHTML = "<table><tr><td id=\"case1\" ></td><td id=\"case2\" ></td><td id=\"case3\" ></td></tr><tr><td id=\"case4\"></td><td id=\"case5\"></t><td id=\"case6\"></td></tr><tr><td id=\"case7\"></td><td id=\"case8\"></td><td id=\"case9\"></td></tr></table>"}, false);
-
-for(i = 1; i < 10; i++)
+function afficheria()
 {
-    tableau[i] = document.getElementById("case"+i);
-    tableau[i].addEventListener("click",CoupJoueur.bind(this, tableau[i],i),false);
-} 
+    document.getElementById("jeu").innerHTML = "En cours de construction";
+}
+
+function afficherjeu()
+{
+    document.getElementById("jeu").innerHTML = "<table><tr><td id=\"case1\" ></td><td id=\"case2\" ></td><td id=\"case3\" ></td></tr><tr><td id=\"case4\"></td><td id=\"case5\"></t><td id=\"case6\"></td></tr><tr><td id=\"case7\"></td><td id=\"case8\"></td><td id=\"case9\"></td></tr></table>";
+    for(i = 1; i < 10; i++)
+    {
+	tableau[i] = document.getElementById("case"+i);
+	tableau[i].addEventListener("click",CoupJoueur.bind(this, tableau[i],i),false);
+    }
+    document.getElementById("jeu").innerHTML = "<div><a id=\"choix1\" href=\"\">Jouer contre l'ordi</a><a id=\"choix2\" href=\"\">2 Joueurs</a></div>";
+}
+
+choix1 = document.getElementById("choix1");
+choix2 = document.getElementById("choix2");
+choix1.addEventListener("click", afficherjeu,false);
+choix2.addEventListener("click", afficheria, false);
+
 alert("syntaxe correcte");
 /*
-1 2 3
+  1 2 3
 4 5 6 
 7 8 9
 */
