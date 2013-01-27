b228806edb53be4bee7a588eb430509d554f5ede
Thomas Salmon <th_s@hotmail.fr>
2013-01-27 18:09:06 +0100

    descente

--- puissance4/main.js
+++ puissance4/main.js
@@ -1,8 +1,23 @@
 var cases = document.getElementsByClassName("case");
 var joueur = true;
 
+function descendre(x)
+{
+    
+}
+
 function f(x)
 {
+    if(cases[x].innerHTML != "&nbsp;")
+    {
+	return ;
+    }
+    
+    while(x+7 < 48 && cases[x+7].innerHTML == "&nbsp;")
+    {
+	x+=7;
+    } 
+    
     if(joueur == true)
     {
 	cases[x].innerHTML= "<div class=\"cercle\"></div>";
@@ -13,6 +28,7 @@ function f(x)
 	cases[x].innerHTML= "<div class=\"cercle2\"></div>";
 	joueur = true;
     }
+    descendre(x);
 }
 
 
