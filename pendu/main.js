8c57fe71bea7a94aeb3ce1a695f42264d408044b
Thomas Salmon <th_s@hotmail.fr>
2013-01-27 12:10:47 +0100

    [pendu] l26

--- pendu/main.js
+++ pendu/main.js
@@ -1,19 +1,31 @@
 var pina_colada = [ "rhum blanc", "jus ananas","creme de coco", "creme fraiche"];
 var mot = pina_colada[Math.floor(Math.random()*pina_colada.length)];
 var choix;
+var pile = "";
 var chaine =  "";
 
 function f(x)
 {
-    if(x.keyCode == 13)
+    if(x.keyCode == 13 && choix.value != "" && pile.search(choix.value) == -1)
     {
+	pile+= choix.value;
+	choix.value ="";
+	chaine = "";
 	for(i = 0; i < mot.length; i++)
 	{
+	    if(pile.search(mot.charAt(i)) == -1)
+	    {
+		chaine = chaine + "_";
+	    }
+	    else
+	    {
+		chaine = chaine + mot.charAt(i);
+	    }
 	}
-    }  
+	document.getElelementById("ecran").innerHTML = chaine;
+	alert(chaine);
+    }
 }
-
-chaine = "";
 for(i = 0; i < mot.length; i++)
 {
     chaine += "_";
@@ -24,4 +36,4 @@ document.getElementById("ecran").innerHTML = chaine;
 choix = document.getElementById("txt");
 choix.addEventListener("keypress", f, false);
 
-//alert("syntaxe correcte");
+alert(mot);
