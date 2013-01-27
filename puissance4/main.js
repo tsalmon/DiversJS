549ba611a25cf94af085a45485e4380ad42a4512
Thomas Salmon <th_s@hotmail.fr>
2013-01-27 18:39:33 +0100

    gagne faute de syntaxe quelque part

--- puissance4/main.js
+++ puissance4/main.js
@@ -1,8 +1,65 @@
 var cases = document.getElementsByClassName("case");
 var joueur = true;
 
-function gagne()
+function estdanstable(x)
 {
+    return (x > 48 || x < 0);
+}
+
+function memecouleur(x,y)
+{
+    return (cases[y].innerHTML.search(cases[x].innerHTML) != -1)
+}
+
+function gagne(x)
+{
+    hd = true;
+    hh = true;
+    hg = true;
+    d = true; 
+    g = true;
+    bg = true;
+    gd = true;
+    bb = true;
+    for(i = 1; i < 4; i++)
+    {
+	if(hd == true && estdanstable(x-8*i) && memecouleur(x,x-8*i))
+	{
+	    hd = false;
+	}
+	if(hh == true && estdanstable(x-7*i) && memecouleur(x,x-7*i))
+	{
+	    hh = false;
+	}
+	if(hg == true && estdanstable(x-6*i) && memecouleur(x,x-6*i))
+	{
+	    hg = false;
+	}
+	if(d == true && estdanstable(x+i) && memecouleur(x,x+i))
+	{
+	    d = false;
+	}
+	if(g == true && estdanstable(x-i) && memecouleur(x,x-i))
+	{
+	    g = false;
+	}
+	if(bg == true && estdanstable(x+6*i) && memecouleur(x,x+6*i))
+	{
+	    bg = false;
+	}
+	if(gd == true && estdanstable(x+8*i) && memecouleur(x,x+8*i))
+	{
+	    gd = false;
+	}
+	if(bb == true && estdanstable(x+7*i) && memecouleur(x,x+7*i))
+	{
+	    alert(bb);
+	    bb = false;
+	}
+
+    }
+    alert(bb);
+    return (hd | hh | hg | d | g | bg | gd | bb );
 }
 
 function f(x)
@@ -12,7 +69,7 @@ function f(x)
 	return ;
     }
     
-    while(x+7 < 48 && cases[x+7].innerHTML == "&nbsp;")
+    while(x+7 < 49 && cases[x+7].innerHTML == "&nbsp;")
     {
 	x+=7;
     } 
@@ -27,7 +84,17 @@ function f(x)
 	cases[x].innerHTML= "<div class=\"cercle2\"></div>";
 	joueur = true;
     }
-    descendre(x);
+    if(gagne(x))
+    {
+	if(joueur == true)
+	{
+	    alert("Le joueur 1 a gagne")
+	}
+	else
+	{
+	    alert("Le joueur 2 a gagne");
+	}
+    }
 }
 
 
