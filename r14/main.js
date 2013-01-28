b90c47191b3010c2e0f4b6d60007a582d8e3fc09
Thomas Salmon <th_s@hotmail.fr>
2013-01-28 23:44:33 +0100

    [r14] affichage

--- r14/main.js
+++ r14/main.js
@@ -5,7 +5,7 @@
   R12 = 6e - 14e places
   R11 = ... R1 = 1/11 
 */
-
+var max ;
 var champs;
 var tableau = new Array();
 
@@ -27,27 +27,50 @@ function decoupe()
     return [str, u[u.length-1]];
 }
 
+function pseudo_existe(x)
+{
+    for(i = 0; i < tableau.length; i++)
+    {
+	if(tableau[i][0] == x)
+	{
+	    return (true);
+	}
+    }
+    return (false);
+}
+
+/* afficher tableau dans la page */
+function affiche()
+{  
+    max = tableau[tableau.length-1][1];
+    str = "";
+    for(i = 0; i < tableau.length ; i++)
+    {
+	str = str + "<tr><td> " +  tableau[i][0] + "</td><td>" + tableau[i][1] + "</td><td>"+i+"</td></tr>";
+    }
+    document.getElementById("tableau").innerHTML = "<table><tr><th>Pseudo</th><th>VH</th><th></th></tr>" + str + "</table>";
+}
+
+/* ajouter le champs dans tableau */
 function ajouter_case(x)
 {
     if(x.keyCode != 13)
     {
 	return ;
     }
-    resultat = decoupe();
+    var resultat = decoupe();
     resultat[1] = parseInt(resultat[1],10);
+    if(pseudo_existe(resultat[0]) == true || resultat[1] == NaN)
+    {
+	return ;
+    }
     tableau.push(resultat);
     tableau = tableau.sort(
 	function(b,c) 
 	{ 
 	    var i=b[1], j=c[1]; if (i===j)return 0; return i<j ? -1 : 1; 
 	});
-    str = "";
-    
-    for(i = 0; i < tableau.length ; i++)
-    {
-	str = str + "<tr><td> " +  tableau[i][0] + "</td><td>" + tableau[i][1] + "</td><td></td></tr>";
-    }
-    document.getElementById("tableau").innerHTML = "<table><tr><th>Pseudo</th><th>VH</th><th></th></tr>" + str + "</table>";
+    affiche();
 }
 
 champs = document.getElementById("champs");
