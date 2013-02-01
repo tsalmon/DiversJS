0fd1a2828f7aa32e1518d4ee8eeeff4e570c60e0
Thomas Salmon <th_s@hotmail.fr>
2013-02-01 12:52:44 +0100

    [pseudo] les bases

--- pseudo/main.js
+++ pseudo/main.js
@@ -1,1 +1,61 @@
+var appui;
 
+function consonne(longueur, lettre)
+{
+    if(longueur == 0)
+    {
+	return "";
+    }
+    switch(lettre)
+    {
+    case 'a':
+	alert("a");
+	break;
+    case 'e':
+	alert("e");
+	break;
+    case 'i':
+
+	break;
+    case 'o':
+	break;
+    case 'u':
+	break;
+    case 'y'
+	break;
+    }
+    return "";
+}
+
+function voyelle(longueur, lettre)
+{
+    if(longueur == 0)
+    {
+	return "";
+    }
+    return "";
+}
+
+function f(lettre, longueur)
+{
+    var x = lettre;
+    if(x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u' || x == 'y')
+    {
+	return lettre + consonne(longueur - 1, lettre); 
+    }
+    else
+    {
+	return lettre + voyelle(longueur -1, lettre); 
+    }
+}
+
+function generer(x)
+{
+    var longueur = Math.floor(Math.random()*5+1);
+    alert(f(String.fromCharCode(Math.ceil(Math.random()*25)+97)));
+}
+
+appui = document.getElementById("genere");
+appui.addEventListener("click", generer, false);
+
+alert("Syntaxe correcte");
