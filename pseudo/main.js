b89fed9baa0830950998ccdad8a6cd70039ac46e
Thomas Salmon <th_s@hotmail.fr>
2013-02-01 14:02:14 +0100

    [pseudo] result

--- pseudo/main.js
+++ pseudo/main.js
@@ -1,5 +1,5 @@
 var appui;
-var syl_con = [ 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', "qu", 'r', 's', 't', 'v', 'w', 'x', 'z', "bh", "ch", "dh", "gh", "jh", "kh", "rh", "th", "wh","zh","ck", "cc", "gu", "st", "nt", "nb"];
+var syl_con = [ 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', "qu", 'r', 's', 't', 'v', 'w', 'x', 'z', "bh", "ch", "dh", "gh", "jh", "kh", "rh", "th", "wh","zh","ck", "gu", "st", "nt", "nb", "cc", "tt", "ff", "mm", "pp", "mm", "ss", "zz", "dd"];
 var syl_voy = [ 'a', 'e', 'i', 'o', 'u', 'y', "ai", "au", "ay", "ei", "ey", "io", "ya", "ye", "yi", "yo", "yu", "eau", "oei"];
 
 function consonne(longueur)
@@ -8,7 +8,7 @@ function consonne(longueur)
     {
 	return "";
     }
-    return syl_con[Math.floor(Math.random() * syl_con.length] + voyelle;
+    return syl_con[Math.floor(Math.random() * syl_con.length)] + voyelle(longueur -1);
 }
 
 function voyelle(longueur)
@@ -17,26 +17,13 @@ function voyelle(longueur)
     {
 	return "";
     }
-    return "";
-}
-
-function f(lettre, longueur)
-{
-    var x = lettre;
-    if(x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u' || x == 'y')
-    {
-	return lettre + consonne(longueur - 1); 
-    }
-    else
-    {
-	return lettre + voyelle(longueur -1); 
-    }
+    return syl_voy[Math.floor(Math.random() * syl_voy.length)] + consonne(longueur -1);
 }
 
 function generer(x)
 {
     var longueur = Math.floor(Math.random()*5+1);
-    alert(f(String.fromCharCode(Math.ceil(Math.random()*25)+97)));
+    document.getElementById("resultat").innerHTML = (Math.random() > 0.5) ? voyelle(Math.floor(Math.random()*6)+1) : consonne(Math.floor(Math.random()*6)+2);
 }
 
 appui = document.getElementById("genere");
