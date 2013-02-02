d4012a348d0e8835ec831b0c7620bcda5f3aefaa
Thomas Salmon <th_s@hotmail.fr>
2013-02-03 00:58:51 +0100

    [couleur] resolut (juste a++ qui merde)

--- couleur/main.js
+++ couleur/main.js
@@ -1,20 +1,18 @@
-
-var c=document.getElementById("myCanvas");
-var ctx=c.getContext("2d");
+var  c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
 var a = 0;
 for(i = 0; i < 256; i++)
 {
     for(j = 0; j < 256; j++)
     {
 	var e = a.toString(16);
-	while(e.length < 6)
+	while( e.length < 6)
 	{
 	    e = 0 + e;
 	}
 	ctx.fillStyle = "#" + e;
-	alert(e);
 	ctx.fillRect(i,j,i+1,j+1);
-	a = a++:
+	a = a + 1;
     }
 }
 alert("syntaxe correcte");
