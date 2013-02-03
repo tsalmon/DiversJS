8dcb75839ecc119684df9f032dc54918ec06365a
Thomas Salmon <th_s@hotmail.fr>
2013-02-03 01:11:47 +0100

    [couleur] 4096 * 4096 c'est un peu trop

--- couleur/main.js
+++ couleur/main.js
@@ -1,9 +1,9 @@
 var  c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 var a = 0;
-for(i = 0; i < 256; i++)
+for(i = 0; i < 256; i+=1)
 {
-    for(j = 0; j < 256; j++)
+    for(j = 0; j < 256; j+=1)
     {
 	var e = a.toString(16);
 	while( e.length < 6)
@@ -11,8 +11,8 @@ for(i = 0; i < 256; i++)
 	    e = 0 + e;
 	}
 	ctx.fillStyle = "#" + e;
-	ctx.fillRect(i,j,i+1,j+1);
-	a = a + 1;
+	ctx.fillRect(i, j, i+1, j+1);
+	a = a + 16;
     }
 }
-alert("syntaxe correcte");
+alert("syntaxe correcte " + a.toString(16));
