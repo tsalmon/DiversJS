e29983412ceeff2bb1e4d4613f50df5c51a36387
Thomas Salmon <th_s@hotmail.fr>
2013-02-10 17:52:51 +0100

    initialise jeu

---
+++ main.js
@@ -0,0 +1,8 @@
+function initialise_jeu()
+{
+    document.getElementById("jeu").innerHTML = "<p><label for=\"j1\">Joueur 1</label><input id=\"j1\" type=\"text\"/></p><p><label for=\"j2\">Joueur 2</label><input id=\"j2\" type=\"text\"/></p><p><label for=\"j3\">Joueur 3</label><input id=\"j3\" type=\"text\"/></p><p><label for=\"j4\">Joueur 4</label><input id=\"j4\" type=\"text\"/></p><p><label for=\"j5\">Joueur 5</label><input id=\"j5\" type=\"text\"/></p><p><label for=\"j6\">Joueur 6</label><input id=\"j6\" type=\"text\"/></p>";
+}
+
+initialise_jeu();
+
+alert("syntaxe correcte");
