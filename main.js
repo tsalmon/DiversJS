bd40bf802e974559b761f1ef9869deaee9fcfaa1
Thomas Salmon <th_s@hotmail.fr>
2013-02-11 01:17:39 +0100

    cases du jeu  : classe ou tableau ?

--- main.js
+++ main.js
@@ -19,6 +19,8 @@ function initialise()
     }
 }
 
+function 
+
 function f(x)
 {
     
@@ -38,6 +40,7 @@ function f(x)
 	if(document.getElementById("j" + i).value.length > 10)
 	{
 	    erreur_init("Les noms ne doivent pas exceder 10 char.");
+	    return ;
 	}
 	if(/^\w+$/.test(document.getElementById("j" + i).value))
 	{
@@ -48,14 +51,10 @@ function f(x)
     if(nb_joueurs < 2)
     {
 	erreur_init("Pas assez de joueurs");
+	return ;
     }
-    else
-    {
-	alert("bon");
-    }
+    alert("inscription validé");
 }
-
-document.getElementById("jeu").innerHTML = "<p><label for=\"j1\">Joueur 1</label><input id=\"j1\" type=\"text\"/></p><p><label for=\"j2\">Joueur 2</label><input id=\"j2\" type=\"text\"/></p><p><label for=\"j3\">Joueur 3</label><input id=\"j3\" type=\"text\"/></p><p><label for=\"j4\">Joueur 4</label><input id=\"j4\" type=\"text\"/></p><p><label for=\"j5\">Joueur 5</label><input id=\"j5\" type=\"text\"/></p><p><label for=\"j6\">Joueur 6</label><input id=\"j6\" type=\"text\"/></p>";
 
 initialise();
 
