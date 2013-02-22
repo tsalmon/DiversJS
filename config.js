f822277a056321b5e6f72df0842c5b8abeec1aa1
Thomas Salmon <th_s@hotmail.fr>
2013-02-22 11:06:45 +0100

    on commence la config d'affichage

---
+++ config.js
@@ -0,0 +1,22 @@
+var radio_defaut = document.getElementById("radio_defaut");
+var radio_joueurs = document.getElementById("radio_joueurs");
+var radio_proprietes = document.getElementById("radio_proprietes");
+
+function affichage_defaut()
+{
+    
+}
+
+function affichage_joueurs()
+{
+    
+}
+
+function affichage_proprietes()
+{
+    
+}
+
+radio_defaut.addEventListener("click", affichage_defaut, false);
+radio_joueurs.addEventListener("click", affichage_joueurs,false);
+radio_proprietes.addEventListener("click", affichage_proprietes, false);
